d37de76716fac64bd509e844842a3b733c5d9d37
EricMTJ <77507530+EricMTJ@users.noreply.github.com>
2022-03-05 13:29:32 -0500

    Update login.php

--- Enrollment_Form/login.php
+++ Enrollment_Form/login.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>User Login Form</title>
 </head>
 <body>
+
+<div>
+    <form action="login.php" method="post">
+        <div class="container">
+            <h1>Enrollment</h1>
+            <p>Please login</p>
+            
+            <label for="firstname"> <b>First Name</b> </label>
+            <input type="text" name="firstname" required>
+
+            <label for="lastname"> <b>Last Name</b> </label>
+            <input type="text" name="lastname" required>
+
+        </div>
+    </form>
+</div>
 
 </body>
 </html>
